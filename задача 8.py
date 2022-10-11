6a0e3a8d594f068d59a44dda4bcf6e7183e61710
Hachu-spatb <113688807+Hachu-spatb@users.noreply.github.com>
2022-10-11 19:28:47 +0300

    Delete задача 8.py

--- задача 8.py
+++
@@ -1,8 +0,0 @@
-def g(a):
-    return a[0]
-with open("input.txt", "r", encoding='utf-8') as f:
-    b = f.readlines()
-    ar = [i.split() for i in b]
-    ars = sorted(ar, key = g)
-    for i in ars:
-        print(*(i[:2] + i[3:4]))
