4602f4eb5c1cf41e3e91351a3144c6488bf70dab
Hachu-spatb <113688807+Hachu-spatb@users.noreply.github.com>
2022-10-11 19:28:23 +0300

    Delete задача 6.py

--- задача 6.py
+++
@@ -1,9 +0,0 @@
-def g(a):
-    return -1 * int(a[3])
-with open("input.txt", "r", encoding='utf-8') as f:
-    b = f.readlines()
-    a = [i.split() for i in b]
-    ar = [[], [], []]
-    for i in a:
-        ar[int(i[2]) - 9].append(i)
-    print(sorted(ar[0], key = g)[0][3], sorted(ar[1], key = g)[0][3], sorted(ar[2], key = g)[0][3])
